e69c39e285baf376742809ddee3e3095e3aacb30
Leonard <aktasm2005@hotmail.com>
2016-01-07 23:29:02 +0200

    67. ders bitti

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Product, Variation, ProductImage, Category
+from .models import Product, Variation, ProductImage, Category, ProductFeatured
 admin.site.register(Product)
 admin.site.register(Variation)
 admin.site.register(ProductImage)
 admin.site.register(Category)
+admin.site.register(ProductFeatured)
